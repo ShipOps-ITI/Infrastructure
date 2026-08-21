b26d8287c60c56c3420d57dea608b296757916f6
ahmedyasser002 <ay012941@gmail.com>
2026-08-21 14:18:56 +0300

    modifying bastion roles

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -85,3 +85,93 @@ resource "aws_iam_role_policy" "bastion_eks" {
   })
 }
 
+# Data for account and region to construct ARNs for backend resources
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+# Inline policy granting access to the Terraform S3 backend and DynamoDB lock table
+resource "aws_iam_role_policy" "bastion_terraform_state" {
+  name = "${var.name}-terraform-state-access"
+  role = aws_iam_role.bastion.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:GetBucketLocation",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::ahmedyasser02-terraform-state",
+          "arn:aws:s3:::ahmedyasser02-terraform-state/dev/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DescribeTable",
+          "dynamodb:Query",
+          "dynamodb:Scan"
+        ]
+        Resource = "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/terraform-locks"
+      }
+    ]
+  })
+}
+
+# Inline read-only policy to allow Terraform plan operations that query AWS resources
+# (EC2 Describe*, IAM GetRole/GetRolePolicy and related read actions). This is
+# intentionally read-only and scoped to the account where resource ARNs require it.
+resource "aws_iam_role_policy" "bastion_terraform_readonly" {
+  name = "${var.name}-bastion-readonly"
+  role = aws_iam_role.bastion.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "iam:GetRole",
+          "iam:GetRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies",
+          "iam:ListRoles"
+        ]
+
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ec2:DescribeImages",
+          "ec2:DescribeAddresses",
+          "ec2:DescribeAddressesAttribute",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeVpcAttribute",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeInstances",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeRouteTables",
+          "ec2:DescribeNatGateways"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+}
+
